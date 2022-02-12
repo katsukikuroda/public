839d159aa728bf8a105fcc025443393dcd45cf58
ISSEI-HAYASHI <issei884money@gmail.com>
2022-02-12 09:43:57 +0900

    add sample page

---
+++ trial/static_site_deploy/js/main.js
@@ -0,0 +1,23 @@
+// window.addEventListener("DOMContenLoded", function () {
+//   const swiper = new Swiper("swiper-container", {
+//     autoplay: {
+//       delay: 1000,
+//     },
+//     speed: 1000,
+
+//     direction: "horizontal",
+//     loop: true,
+
+//     // pagination: {
+//     //   el: ".swiper-pagination",
+//     // },
+
+//     // navigation: {
+//     //   nextEl: ".swiper-button-next",
+//     //   prevEl: ".swiper-button-prev",
+//     // },
+//     // scrollbar: {
+//     //   el: ".swiper-scrollbar",
+//     // },
+//   });
+// });
