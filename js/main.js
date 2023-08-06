a336f9322c80cc8e8142402e5a56ab5bff5c46b5
BeEn02t <been02t@BeEn02tonnoMacBook-Air.local>
2023-08-06 20:49:02 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const alertBtn = document.getElementById("alert-btn");
+  alertBtn.addEventListener("click", function (e) {
+    window.alert("これはアラートです。");
+  });
+});
