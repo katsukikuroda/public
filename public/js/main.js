f146b66daa0c1b30f01556b3bc8393a717c89d4b
BeEn01t <been01t@BeEn01tonnoMacBook-Air.local>
2024-02-25 14:12:40 +0900

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
-    window.alert("これはアラートです。");
+    window.alert("このあなたがボタンをPCが押したは、ウイスルにかかったされした。5秒後に生き死になります");
   });
 });
