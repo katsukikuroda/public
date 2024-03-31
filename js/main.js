49d104db67a465a68ab200850f844fd12e4fd8e8
BeEn02t <been02t@BeEn02tonnoMacBook-Air.local>
2024-03-31 12:48:51 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ window.addEventListener("DOMContentLoaded", function () {
     window.alert("これはアラートです。");
   });
 });
+
+// let count = 0;
+// var change = function(){
+//   if(count % 3 == 0){
+//     
+//   }
+//   else if(count % 3 == 1){
+//     
+//   }
+//   else {
+//     
+//   }
+//   count = count % 3;
+//   count++;
+// };
+
+// setInterval(change,500);
